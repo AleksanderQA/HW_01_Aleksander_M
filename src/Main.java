9771f1e7b0aacb728f481bbcfbda4d586ee8896c
Alexander Medvedev <razerguy999@gmail.com>
2024-01-26 20:31:11 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("\u001B[31m" + "RED" + "\u001B[0m");
+        System.out.println("\u001B[34m" + "BLUE" + "\u001B[0m");
+        System.out.println("\u001B[30m" + "BLACK" + "\u001B[0m");
+
+        System.out.println("\u001B[31m"+"     *" + "\u001B[0m");
+        System.out.println("    ***");
+        System.out.println("   *****");
+        System.out.println("  *******");
+        System.out.println(" *********");
+        System.out.println("***********");
+        System.out.println(" *********");
+        System.out.println("  *******");
+        System.out.println("   *****");
+        System.out.println("    ***");
+        System.out.println("\u001B[31m"+"     *" + "\u001B[0m");
+
+    }
+}
